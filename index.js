c2e5e8a8285841f75eb802eb2db0e38aa4153488
KOLEX <66454317+kolexkolawole@users.noreply.github.com>
2022-08-02 04:58:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 alert("Welcome to my first JavaScript enabled page");
 console.log("My name is Akanni Kolawole .O");
 let myFullName = 'Name: Akanni Kolawole .O';
-let myHeight = 'Height: 170cm';
+let myHeight = 'Height: 185cm';
 let myCountry = 'Countr: Nigeria';
 
 console.log(myFullName)
@@ -9,5 +9,5 @@ console.log(myHeight)
 console.log(myCountry)
 
 document.getElementById("javascript").innerHTML = "Name: Akanni Kolawole .O";
-document.getElementById("javascript1").innerHTML = "Height: 170cm";
+document.getElementById("javascript1").innerHTML = "Height: 185cm";
 document.getElementById("javascript2").innerHTML = "Country: Nigeria";
